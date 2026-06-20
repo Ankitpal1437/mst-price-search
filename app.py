19f3e836f6dc93526b93e619a72ca179989123c8
Ankitpal1437 <ank123it45678@gmail.com>
2026-06-20 14:44:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
+# CSV load
+df = pd.read_csv("price.csv")
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    results = []
+
+    if request.method == "POST":
+
+        search = request.form["search"].strip().lower()
+
+        for _, row in df.iterrows():
+
+            try:
+                code = str(row.iloc[0]).lower()
+                desc = str(row.iloc[1]).lower()
+
+                if search in code or search in desc:
+                    results.append(row)
+
+                if len(results) >= 10:
+                    break
+
+            except:
+                pass
+
+    return render_template("index.html", results=results)
 
 if __name__ == "__main__":
     app.run()
